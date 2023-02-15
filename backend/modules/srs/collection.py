dc5ae729a32970d3374b31dca67311ed8602cd99
ctrlaltf2 <ctrlaltf2@protonmail.ch>
2023-02-14 23:00:23 -0500

    Add scheduler abstract class

--- backend/modules/srs/collection.py
+++ backend/modules/srs/collection.py
@@ -3,7 +3,11 @@ from modules.srs.track import Track
 
 # Class to represent a set of learning tracks
 class Collection:
+    # List of the tracks
     tracks: list[Track]
+
+    # Index of the active track
+    _active_track_index: int = 0
 
     def __init__(self, tracks: list[Track]):
         self.tracks = tracks
@@ -13,3 +17,16 @@ class Collection:
 
     def __repr__(self):
         return f"Collection(\n\ttracks={str(self.tracks)}\n)"
+
+    def get_active_track(self) -> Track:
+        return self.tracks[self._active_track_index]
+
+    def set_active_track(self, identifier: str | int):
+        if type(identifier) is str:
+            # Find the track with the matching name
+            for i, track in enumerate(self.tracks):
+                if track.name == identifier:
+                    self._active_track_index = i
+                    break
+        elif type(identifier) is int:
+            self._active_track_index = identifier
