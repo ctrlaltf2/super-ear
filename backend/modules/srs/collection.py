aaf1b93a03dc825179b3f2ea7eb2b5fbf37e0e81
ctrlaltf2 <ctrlaltf2@protonmail.ch>
2023-02-19 18:27:12 -0500

    Implement most of SRS algorithm

--- backend/modules/srs/collection.py
+++ backend/modules/srs/collection.py
@@ -16,11 +16,36 @@ class Collection:
     # Epoch for the collection
     epoch: datetime.datetime
 
+    # Day of most recent review. Days since epoch.
+    last_review_day: int = 0
+
+    # Number of new items left to study on current day (epoch + last_review_day)
+    n_new_items_today: int
+
+    # Number of reviewing items left to study on current day (epoch + last_review_day)
+    n_review_items_today: int
+
+    # -- collection settings
+
+    # Maximum number of new items to study per day
+    max_new_per_day: int = 3
+
+    # Maximum number of items to study per day. # of cards, not # of reviews.
+    max_reviews_per_day: int = 75
+
+    # If should mix review and new items together
+    do_mix_new_review: bool = False
+
     def __init__(self, tracks: list[Track], **kwargs):
         self.tracks = tracks
 
+        # Pull in configuration
         self.active_track_index = kwargs.get("active_track_index", 0)
         self.epoch = kwargs.get("epoch", datetime.datetime.now())
+        self.max_new_per_day = kwargs.get("max_new_per_day", 3)
+        self.max_reviews_per_day = kwargs.get("max_reviews_per_day", 75)
+        self.do_mix_new_review = kwargs.get("do_mix_new_review", False)
+        self.last_review_day = kwargs.get("last_review_day", 0)
 
     def add(self, item: Track):
         self.tracks.append(item)
