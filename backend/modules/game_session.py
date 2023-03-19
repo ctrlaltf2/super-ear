56567b37b80d7abfb2e22f63fe83342beb058161
ctrlaltf2 <ctrlaltf2@protonmail.ch>
2023-03-19 17:20:46 -0400

    Implement pairing state, add messages to some asserts

--- backend/modules/game_session.py
+++ backend/modules/game_session.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import json
 
+from enum import Enum
 from typing import Callable
 
 import tornado.websocket
@@ -15,6 +16,13 @@ if TYPE_CHECKING:
 
 
 class GameSessionSocketHandler(tornado.websocket.WebSocketHandler):
+    class SessionState(Enum):
+        WAITING_FOR_DSP = "waiting_for_dsp"
+        PAIRED = "paired"
+
+        def __str__(self):
+            return self.value
+
     # on_open is a function callback
     cb_on_open: Callable
 
@@ -28,7 +36,10 @@ class GameSessionSocketHandler(tornado.websocket.WebSocketHandler):
     dsp_session: DSPSession | None
 
     # Pairing code, assigned by SuperEarApplication
-    _pair_code: list[int]
+    _pair_code: list[int] | None
+
+    # State of the session
+    _state: SessionState
 
     def __init__(self, *args, **kwargs):
         print("GameSocket::__init__()")
@@ -52,9 +63,14 @@ class GameSessionSocketHandler(tornado.websocket.WebSocketHandler):
         del kwargs["on_message"]
 
         self.dsp_session = None
+        self._pair_code = None
 
         # call parent ctor
         super().__init__(*args, **kwargs)
+
+    def _set_state(self, state: SessionState):
+        self._state = state
+        self.send_frontend_message("state", str(state))
 
     def get_compression_options(self):
         # Non-None enables compression with default options.
@@ -72,12 +88,18 @@ class GameSessionSocketHandler(tornado.websocket.WebSocketHandler):
 
         self.sock = self.ws_connection.stream.socket.getpeername()
 
+        self._set_state(self.SessionState.WAITING_FOR_DSP)
+        assert (
+            self._state == self.SessionState.WAITING_FOR_DSP
+        ), "post: set state should set the state"
+
         self.cb_on_open(self.sock, self)
 
     def on_close(self) -> None:
         print("WebSocket closed")
         # TODO: Signal unpair to connected DSP
         self.cb_on_close(self.sock)
+        self.unpair()
 
     def on_message(self, message: str) -> None:
         assert self.ws_connection is not None
@@ -107,22 +129,42 @@ class GameSessionSocketHandler(tornado.websocket.WebSocketHandler):
 
         self.cb_on_message(self.sock, message)
 
-    def pair(self, dsp_session: DSPSession) -> None:
-        assert self.dsp_session is None
-        self.dsp_session = dsp_session
-        self.send_to_dsp("Hello from game session")
-
     def assign_pair_code(self, pair_code: list[int]):
+        assert self._pair_code is None
         self._pair_code = pair_code
 
     def send_to_dsp(self, msg: str):
+        print(f"Sending to DSP: {msg}")
         if self.dsp_session is None:
             return
 
+        assert self._state == self.SessionState.PAIRED
+
         self.dsp_session.send_message(msg)
+
+    def pair(self, dsp_session: DSPSession) -> None:
+        print("GameSessionSocketHandler::pair()")
+        assert self.dsp_session is None, "Already paired"
+        print("GameSessionSocketHandler::pair() - 2")
+        self.dsp_session = dsp_session
+        self._set_state(self.SessionState.PAIRED)
+        print("GameSessionSocketHandler::pair() - 5")
+
+        assert (
+            self._state == self.SessionState.PAIRED
+        ), "post: set state should set the state"
+        print("GameSessionSocketHandler::pair() - 3")
+
+        self.send_to_dsp("Hello from game session")
+        print("GameSessionSocketHandler::pair() - 4")
 
     def unpair(self):
         self.dsp_session = None
+        self._set_state(self.SessionState.WAITING_FOR_DSP)
+
+        assert (
+            self._state == self.SessionState.WAITING_FOR_DSP
+        ), "post: set state should set the state"
 
     # Sends a message to the connected frontend client
     def send_frontend_message(self, type: str, data: float | int | str | list | dict):
