708ec90484ccc916ec70f479f08927ea7f99ab48
ctrlaltf2 <23644849+ctrlaltf2@users.noreply.github.com>
2023-03-21 01:33:39 -0400

    Add initial user data model

---
+++ backend/app/models/user.py
@@ -0,0 +1,20 @@
+import uuid
+
+from beanie import Document
+from pydantic import UUID4
+from pydantic.fields import Field
+
+from app.models import Collection, DefaultCollections
+
+
+class User(Document):
+    uuid: UUID4 = Field(default_factory=uuid.uuid4, description="The user's UUID")
+
+    username: str = Field(
+        ..., description="The user's username", max_length=32, min_length=4
+    )
+
+    collection: Collection = Field(
+        default_factory=lambda: DefaultCollections.get("GuitarStandard"),
+        description="The collection of the user. Future: Will support multiple.",
+    )
