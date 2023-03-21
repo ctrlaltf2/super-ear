eea2cbac10cbafc5e6938534f6f5a269d4cefdf3
ctrlaltf2 <23644849+ctrlaltf2@users.noreply.github.com>
2023-03-21 13:10:25 -0400

    Get some semblence of DB connection working
    auth is still broken for mysterious reasons

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -5,11 +5,15 @@ class Settings(BaseSettings):
     PROJECT_NAME: str = "Super Ear"
     DEBUG: bool = True
 
-    MONGODB_URI: MongoDsn = "mongodb://mongo:27017"  # type: ignore[assignment]
+    MONGODB_HOST: str = "mongo:27017"  # type: ignore[assignment]
     MONGODB_DB_NAME: str = "super_ear"
 
     MONGO_INITDB_ROOT_USERNAME: str
     MONGO_INITDB_ROOT_PASSWORD: str
+
+    @property
+    def MONGODB_URI(self) -> MongoDsn:
+        return f"mongodb://{self.MONGO_INITDB_ROOT_USERNAME}:{self.MONGO_INITDB_ROOT_PASSWORD}@{self.MONGODB_HOST}"  # type: ignore[assignment]
 
 
 settings = Settings(_env_file=".env", _env_file_encoding="utf-8")
--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,4 @@
+import asyncio
 import logging
 
 import tornado.web
@@ -7,23 +8,25 @@ import tornado.ioloop
 from tornado.options import options, define
 
 from app.super_ear import SuperEarApplication
-from app.core.dsp_server import DSPServer
+from app.db.init_db import init
 
 
 define("debug", default=False, help="Debug mode for the application")
 logger = logging.getLogger(__name__)
 
 
-def tcpServer():
-    server = DSPServer()
-    server.listen(options.dsp_port)
-    return server
-
-
 if __name__ == "__main__":
     options.parse_command_line()
+
+    # setup DB
+    asyncio.get_event_loop().run_until_complete(init())
+
+    # Setup main app
     app = SuperEarApplication()
 
+    # setup HTTP server
     server = tornado.httpserver.HTTPServer(app)
     server.listen(8000)
+
+    # run
     tornado.ioloop.IOLoop.current().start()
