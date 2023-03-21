bdaffc784d9ba0f835fe79944e99e1c22453b258
ctrlaltf2 <23644849+ctrlaltf2@users.noreply.github.com>
2023-03-21 01:25:29 -0400

    Port 6-string standard tuning trainer collection to ODM

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,64 @@
+from .review_item import ReviewItem
+from .track import Track
+from .collection import Collection
+from app.core.srs.spn import SPN
+
+
+# -- Default collections
+class DefaultCollections:
+    # Standard tuning, guitar, 6-string. 21 notes
+    _GuitarStandard = Collection(
+        tracks=[
+            Track.from_list(
+                [
+                    ReviewItem(SPN.from_str("E4") + offset)
+                    for offset in range(0, 21 + 1)
+                ],
+                "String 1",
+            ),
+            Track.from_list(
+                [
+                    ReviewItem(SPN.from_str("B3") + offset)
+                    for offset in range(0, 21 + 1)
+                ],
+                "String 2",
+            ),
+            Track.from_list(
+                [
+                    ReviewItem(SPN.from_str("G3") + offset)
+                    for offset in range(0, 21 + 1)
+                ],
+                "String 3",
+            ),
+            Track.from_list(
+                [
+                    ReviewItem(SPN.from_str("D3") + offset)
+                    for offset in range(0, 21 + 1)
+                ],
+                "String 4",
+            ),
+            Track.from_list(
+                [
+                    ReviewItem(SPN.from_str("A2") + offset)
+                    for offset in range(0, 21 + 1)
+                ],
+                "String 5",
+            ),
+            Track.from_list(
+                [
+                    ReviewItem(SPN.from_str("E2") + offset)
+                    for offset in range(0, 21 + 1)
+                ],
+                "String 6",
+            ),
+        ]
+    )
+
+    @staticmethod
+    def get(name: str):
+        from copy import deepcopy
+
+        if name == "GuitarStandard":
+            return deepcopy(DefaultCollections._GuitarStandard)
+
+        raise ValueError(f"Collection {name} not found.")
