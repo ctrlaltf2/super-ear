e45c480eccb09f45be15a59c6cb68ac53a5f44c2
ctrlaltf2 <ctrlaltf2@protonmail.ch>
2023-03-21 13:10:25 -0400

    Get some semblence of DB connection working
    auth is still broken for mysterious reasons

--- backend/app/db/init_db.py
+++ backend/app/db/init_db.py
@@ -1,6 +1,16 @@
 from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
 
+from app.core.config import settings
+from app.models.user import User
+
 
 async def init() -> None:
-    pass
+    print("Initializing to MongoDB @", settings.MONGODB_URI)
+
+    client = AsyncIOMotorClient(str(settings.MONGODB_URI))
+
+    await init_beanie(
+        database=client[settings.MONGODB_DB_NAME],
+        document_models=[User],
+    )
