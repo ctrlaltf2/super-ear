cf5a0d89fc4f603432b1a211ef62e59a916e9d5e
ctrlaltf2 <ctrlaltf2@protonmail.ch>
2023-03-21 01:18:28 -0400

    Port SRS data structures to ODM

--- backend/app/models/review_item.py
+++ backend/app/models/review_item.py
@@ -1,7 +1,8 @@
 from enum import Enum, auto
-from typing import Any, Optional, Self
+from typing import Optional
 
 from pydantic import BaseModel
+from pydantic.fields import Field
 
 
 # Enum for the three states of a review item; unseen, learning, and reviewing
@@ -12,47 +13,46 @@ class ReviewState(Enum):
 
 
 # Class to represent a reviewable item and its associated scheduler parameters
-class ReviewItem:
+class ReviewItem(BaseModel):
     # Current state of the review item
-    state: ReviewState
+    state: ReviewState = Field(ReviewState.Unseen, description="review state")
 
-    # Current learning step
-    learning_index: int
+    # Current learning step index
+    learning_index: int = Field(0, ge=0, description="current learning index")
 
     # Learning steps for the item (in minutes)
-    learning_steps: list[float]
+    # default steps from recommended settings of https://ankiweb.net/shared/info/1250336138 (A ear training Anki deck)
+    learning_steps: list[float] = Field(
+        default_factory=lambda: [1 / 12, 5 / 12, 2, 10, 60, 300],
+        description="learning steps, in minutes",
+    )
 
     # ReviewItem content
-    content: Any
+    content: str = Field(..., description="content of the review item")
 
     # Last review time (minutes delta since collection's epoch)
-    last_review: Optional[int]
+    last_review: Optional[int] = Field(
+        description="last review time. minutes delta since collection epoch."
+    )
 
     # Ease-factor / difficulty of the item, captured as a float. No dimensions.
     # Initialized to 2.5, since that's the default in Anki & SM-2.
-    ease_factor: float
+    ease_factor: float = Field(
+        2.5, description="ease factor. estimates difficulty of the item"
+    )
 
     # The current interval, in days
-    current_interval: Optional[float]
+    current_interval: Optional[float] = Field(description="current interval, in days")
 
     # Note: No time delta is used here because the scheduler will be
     # responsible and calculate it during each review session
 
-    def __init__(self, content: Any, **kwargs):
-        self.state = kwargs.get("state", ReviewState.Unseen)
-        self.learning_index = kwargs.get("learning_index", 0)
-        self.learning_steps = kwargs.get(
-            "learning_steps", [1 / 12, 5 / 12, 2, 10, 60, 300]
-        )  # default steps from recommended settings of https://ankiweb.net/shared/info/1250336138 (A ear training Anki deck)
-        self.content = content
-        self.last_review = kwargs.get("last_review", None)
-        self.ease_factor = kwargs.get("ease_factor", 2.5)
-        self.current_interval = kwargs.get("current_interval", None)
-
     # equality comparisons are based on the content,
     # not the scheduler parameters
+    """
     def __eq__(self, other: Self) -> bool:
         return self.content == other.content
+    """
 
     def __str__(self):
         return str(self.content)
