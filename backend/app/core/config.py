b24ad165fdde03f28981a613a64acf84142cdb9c
ctrlaltf2 <23644849+ctrlaltf2@users.noreply.github.com>
2023-03-21 00:37:16 -0400

    Add secrets management via env files

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,1 +1,15 @@
 from pydantic import BaseSettings, MongoDsn
+
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "Super Ear"
+    DEBUG: bool = True
+
+    MONGODB_URI: MongoDsn = "mongodb://mongo:27017"  # type: ignore[assignment]
+    MONGODB_DB_NAME: str = "super_ear"
+
+    MONGO_INITDB_ROOT_USERNAME: str
+    MONGO_INITDB_ROOT_PASSWORD: str
+
+
+settings = Settings(_env_file=".env", _env_file_encoding="utf-8")
