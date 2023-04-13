10a816723983958c53624707244bc6905535186e
ctrlaltf2 <ctrlaltf2@protonmail.ch>
2023-04-13 12:01:17 -0400

    Add initial synth

--- frontend/src/components/PlayButton.jsx
+++ frontend/src/components/PlayButton.jsx
@@ -1,5 +1,6 @@
 import {NavLink} from "react-router-dom"
 import guitar from "../components/pics/guitar.png";
+import * as Tone from 'tone'
 
 export const PlayButton = (props) => {
     return (
@@ -9,7 +10,13 @@ export const PlayButton = (props) => {
         <div className="relative group">
             <div className="absolute -inset-1 bg-gradient-to-b from-blue-400 to-blue-800 rounded-full blur-xl opacity-50 group-hover:opacity-100 transition duration-1000 group-hover:duration-1000"></div>
             <NavLink to="/play">
-                <button className="relative w-24 h-24 rounded-full bg-blue-500 shadow-sm group-hover:scale-110 duration-1000 group-hover:duration-1000">
+                <button
+                    onclick={async () => {
+                        print('waiting')
+                        await Tone.start()
+                        console.log('audio is ready')
+                    }}
+                    className="relative w-24 h-24 rounded-full bg-blue-500 shadow-sm group-hover:scale-110 duration-1000 group-hover:duration-1000">
                     <img className="group-hover:opacity-75 duration-1000 group-hover:duration-1000"
                         src={guitar}
                     >
